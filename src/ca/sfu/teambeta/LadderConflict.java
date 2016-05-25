1ffd7f9cba978d97ee193a5644c73d8b68b698dc
David Li <dla133@sfu.ca>
2016-05-24 22:40:35 -0700

    Add LadderConflict.java and Team.java

---
+++ src/ca/sfu/teambeta/LadderConflict.java
@@ -0,0 +1,8 @@
+package ca.sfu.teambeta;
+
+public class LadderConflict {
+    public static void main (String[] args) {
+        Team team = new Team("david", "dave");
+        String.println(team);
+    }
+}
