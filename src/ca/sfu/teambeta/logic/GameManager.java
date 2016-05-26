5e81f707118d148bfbd4c931b5d17551d9838dd0
nra22 <nra22@sfu.ca>
2016-05-25 18:12:09 -0700

    Added to Game Class

--- src/ca/sfu/teambeta/logic/GameManager.java
+++ src/ca/sfu/teambeta/logic/GameManager.java
@@ -4,4 +4,32 @@ package ca.sfu.teambeta.logic;
  * Created by Gordon Shieh on 25/05/16.
  */
 public class GameManager {
+
+    private MatchCard testCard;
+
+    public GameManager() {
+
+        // just testing it out. Can have an ArrayList here to store Match Cards.
+        testCard = new MatchCard(3);
+    }
+
+    public void createMatchCards() {
+
+        //Teams would be gathered from the Ladder.
+        testCard.addTeamToMatchCard("Roger Federer");
+        testCard.addTeamToMatchCard("Rafa Nadal");
+        testCard.addTeamToMatchCard("Novak Djokovic");
+
+        testCard.displayMatchCard();
+    }
+
+    public void inputMatchResults() {
+
+        testCard.addMatchCardResults(1, "W", "L", "NA");
+        testCard.addMatchCardResults(2, "W", "NA", "L");
+
+        System.out.println();
+        testCard.displayMatchCard();
+    }
+
 }
