70e2264eefea8054456f3eec9f36eadf7384b008
Gordon Shieh <gshieh@sfu.ca>
2016-05-27 15:54:37 -0700

    Implemented bare basics of Player class

--- src/ca/sfu/teambeta/core/Player.java
+++ src/ca/sfu/teambeta/core/Player.java
@@ -4,4 +4,35 @@ package ca.sfu.teambeta.core;
  * Created by Gordon Shieh on 25/05/16.
  */
 public class Player {
+    private int playerID;
+    private String name;
+
+    Player(int id, String name) {
+        this.playerID = id;
+        this.name = name;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return playerID == player.playerID;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return playerID;
+    }
 }
