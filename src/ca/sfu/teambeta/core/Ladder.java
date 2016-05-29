650cf8afa1356722deb2bbc018efdd360faf49ea
Kostiantyn Koval <constantin@invictus>
2016-05-29 14:47:15 -0700

    LadderManager move to logic folder. Restructure of Ladder and LadderManager

--- src/ca/sfu/teambeta/core/Ladder.java
+++ src/ca/sfu/teambeta/core/Ladder.java
@@ -7,72 +7,22 @@ import java.util.List;
  * Created by Gordon Shieh on 25/05/16.
  */
 public class Ladder {
-    //Ladder is subdivided into two Lists: playing and not playing Pairs
-    //Groups are created from playing(active) pairs in GameManager
-    //Members are a total amount of all pairs = active + passive
     private List<Pair> passivePairs;
-    private List<Pair> activePairs;
-    private List<List<Pair>> groups;
-    private int active;
-    private int passive;
+    private int members;
 
     public Ladder(){
-        active = 0;
-        passive = 0;
-        activePairs = new ArrayList<>();
+        //members;
         //passivePairs from the DB
-        groups = new ArrayList<List<Pair>>();
     }
 
-    public List<Pair> getActivePair(){
+    public List<Pair> getPassivePairs(){
         return passivePairs;
-    }
-
-    public List<Pair> getPassivePair(){
-        return passivePairs;
-    }
-
-    public void putGroups(ArrayList<List<Pair>> groups){
-        this.groups = groups;
-    }
-
-    public List<List<Pair>> getGroups(){
-        return groups;
-    }
-
-    public void setIsPlaying(Pair somePair){
-        if (!activePairs.contains(somePair)) {
-            int index = somePair.getPosition();
-            activePairs.add(index, somePair);
-            passivePairs.remove(somePair);
-            passive--;
-            active++;
-        }
-    }
-
-    public void setNotPlaying(Pair somePair){
-        if (!passivePairs.contains(somePair)) {
-            int index = somePair.getPosition();
-            passivePairs.add(index, somePair);
-            activePairs.remove(somePair);
-            active--;
-            passive++;
-        }
-    }
-
-    public void addNewPair(Pair newPair){
-        newPair.setPosition(passive);
-        passivePairs.add(newPair);
-        passive++;
     }
 
     public void removePair(Pair pair){
-        if ( ! activePairs.remove(pair)){
-            if (passivePairs.remove(pair)){
-                passive--;
-            }
-        }else{
-            active--;
+        if (passivePairs.remove(pair)){
+            members--;
+            //UPDATE
         }
     }
 
@@ -81,18 +31,27 @@ public class Ladder {
             if (current.hasPlayer(firstPlayer, secondPlayer)){
                 int iPair = current.getPosition();
                 passivePairs.remove(iPair);
-                passive--;
+                //UPDATE
+                members--;
                 break;
             }
         }
-        for (Pair current : activePairs){
-            if (current.hasPlayer(firstPlayer, secondPlayer)){
-                int iPair = current.getPosition();
-                activePairs.remove(iPair);
-                active--;
+    }
+
+    public void insert(int position, Pair pair){
+        int i = 0;
+        boolean inserted = false;
+        for (Pair current : passivePairs){
+            if (current.getPosition() > position){
+                passivePairs.add(i, pair);
+                inserted = true;
                 break;
             }
         }
+        if (! inserted){
+            passivePairs.add(pair);
+        }
+        members++;
     }
 
     public void swapPair(int fromIndex, int toIndex){
@@ -102,7 +61,7 @@ public class Ladder {
         int secondPosition = secondPair.getPosition();
 
         firstPair.setPosition(secondPosition);
-        firstPair.setPosition(firstPosition);
+        secondPair.setPosition(firstPosition);
 
         passivePairs.add(fromIndex, secondPair);
         passivePairs.add(toIndex, firstPair);
@@ -112,19 +71,11 @@ public class Ladder {
         passivePairs = newLadder;
     }
 
-    public void makeActiveEmpty(){
-        activePairs.clear();
-    }
-
-    public int sizePlaying(){
-        return active;
-    }
-
-    public int sizeNotPlaying(){
-        return passive;
+    public void increaseSize(){
+        members++;
     }
 
     public int size(){
-        return active+passive;
+        return members;
     }
 }
