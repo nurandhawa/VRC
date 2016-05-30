92f264a23d56c14eacf648fb175e6093f272f2c3
Alex Land <aland@sfu.ca>
2016-05-30 09:47:44 -0700

    Rebased latest GameManager changes, fixed integration with updated Player class

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,12 +1,14 @@
 package ca.sfu.teambeta;
 
 import ca.sfu.teambeta.logic.GameManager;
+import ca.sfu.teambeta.ui.UserInterface;
 
 class Main {
     public static void main(String args[]) {
 
+        GameManager newGame = new GameManager();
+
+        UserInterface.start();
+
     }
 }
-
-
-
