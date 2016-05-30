45dd9ce1926d7e412f6efd66b5cef3faf1e4edc7
David Li <dla133@sfu.ca>
2016-05-30 00:24:07 -0700

    Update Player with player name.

--- src/ca/sfu/teambeta/core/Player.java
+++ src/ca/sfu/teambeta/core/Player.java
@@ -4,4 +4,13 @@ package ca.sfu.teambeta.core;
  * Created by Gordon Shieh on 25/05/16.
  */
 public class Player {
+    private String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
 }
