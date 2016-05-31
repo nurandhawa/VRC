ac06c5adeae090b5c86a8405c826170d02f6a0de
David Li <dla133@sfu.ca>
2016-05-31 12:59:32 -0700

    Refactor to getActivePairs()

---
+++ test/ca/sfu/teambeta/logic/LadderManagerTest.java
@@ -0,0 +1,40 @@
+package ca.sfu.teambeta.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by David Li on 30/05/16.
+ */
+public class LadderManagerTest extends TestCase{
+
+    List<Pair> pairList;
+    pairList = new ArrayList(){{
+        add(new Pair(new Player(1, "David1"), new Player(2, "Dave1"), true));
+        add(new Pair(new Player(3, "David1"), new Player(4, "Dave1"), false));
+        add(new Pair(new Player(5, "David1"), new Player(6, "Dave1"), false));
+        add(new Pair(new Player(7, "David1"), new Player(8, "Dave1"), true));
+    }};
+
+    @Test
+    public void testFindActivePairs() {
+
+        LadderManager ladderManager;
+        ladderManager = new LadderManager();
+        ladderManager.init(pairList);
+
+        List<Pair> activePairs = new ArrayList(){{
+            add(new Pair(new Player(1, "David1"), new Player(2, "Dave1"), true));
+            add(new Pair(new Player(7, "David1"), new Player(8, "Dave1"), true));
+        }};
+
+        Assert.assertEquals(ladderManager.getActivePlayers(), expectedOrder);
+    }
+    @Test
+    public void testFindPassivePlayers() {
+
+    }
+}
