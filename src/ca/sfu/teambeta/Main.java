6bfa718dc5c1d3d464870219de7a5772a6dafff9
Alex Land <aland@sfu.ca>
2016-05-31 08:28:56 -0700

    Integrate LadderManager methods (still need a getLadder method), added GameManager (still need match input method, editMatchmethod, and getMatches method).

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,14 +1,15 @@
 package ca.sfu.teambeta;
 
 import ca.sfu.teambeta.logic.GameManager;
+import ca.sfu.teambeta.logic.LadderManager;
 import ca.sfu.teambeta.ui.UserInterface;
 
 class Main {
     public static void main(String args[]) {
 
-        GameManager newGame = new GameManager();
-
-        UserInterface.start();
+        GameManager gameManager = new GameManager();
+        LadderManager ladderManager = new LadderManager();
+        UserInterface.start(gameManager, ladderManager);
 
     }
 }
