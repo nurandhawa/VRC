6daea430aa720460369cf76c2cea59fee425a4e0
jasdeep-jassal <jjassal@sfu.ca>
2016-06-01 17:29:43 -0700

    Refactored and added some error checking

--- test/ca/sfu/teambeta/core/TestObjects.java
+++ test/ca/sfu/teambeta/core/TestObjects.java
@@ -15,9 +15,9 @@ public class TestObjects {
     private List<Player> players;
     private List<Pair> pairs;
 
-    public void TestObjects() {
-        players = new ArrayList<Player>();
-        pairs = new ArrayList<Pair>();
+    public TestObjects() {
+        players = new ArrayList<>();
+        pairs = new ArrayList<>();
 
         setupNames();
         setupPlayers();
@@ -35,24 +35,34 @@ public class TestObjects {
     }
 
 
-    // MARK: - Functions to setup our array's
+    // MARK: - Methods to setup our array's
     private void setupNames() {
-        // Note: If adding a name to this array, make sure you add two names. Or else Pair creation will be thrown off
+        // NOTE: If adding a name to this array, make sure you add two names. Or else Pair creation will be thrown off
         //  IE: Keep it an even number of names
 
         names = new ArrayList<String>(Arrays.asList("Shikoba", "Lori", "Brant", "Mikki", "Kasandra", "Netta", "Dorita",
-                "Lewis", "Nikolas", "Emmanuel", "Alvina", "Linette", "Dion", "Abegail"));
+                "Lewis", "Nikolas", "Emmanuel", "Alvina", "Linette", "Dion", "Sidney"));
     }
 
     private void setupPlayers() {
         for (int i = 0; i < names.size(); i++) {
-            Player player = new Player(i, names.get(i));
+            Player player = new Player(i + 1, names.get(i));
 
             players.add(player);
         }
     }
 
     private void setupPairs() {
+        if (players.size() % 2 != 0) {
+            System.out.println("ERROR: Odd number of players");
+
+            // Gracefully handle this error by adding a placeholder name indicating the error
+            //  Generally we won't reach this condition because the add function will prompt for two names
+            String errorName = "ERR-ADD_NAME";
+            this.names.add(errorName);
+            this.players.add(new Player(this.names.size(), errorName));
+        }
+
         for (int i = 0; i < players.size(); i += 2) {
             Player player1 = players.get(i);
             Player player2 = players.get(i + 1);
@@ -61,5 +71,39 @@ public class TestObjects {
 
             pairs.add(pair);
         }
+    }
+
+
+    // MARK: - Method to
+
+    // MARK: - Printing Methods
+    public void printPlayers() {
+        System.out.println("\nPlayers: (ID & Name)");
+        // Print all names
+        for (Player player : this.players) {
+            System.out.println("" + player.getPlayerID() + " : " + player.getName());
+        }
+    }
+
+    public void printPairs() {
+        System.out.println("\nPairs:");
+        System.out.println("------");
+        // Print all names
+
+        int i = 1;
+        for (Pair pair : this.pairs) {
+            System.out.println("Pair #: " + i);
+            System.out.println(pair + "\n");
+
+            i += 1;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        TestObjects testObjs = new TestObjects();
+
+        testObjs.printPlayers();
+        testObjs.printPairs();
     }
 }
