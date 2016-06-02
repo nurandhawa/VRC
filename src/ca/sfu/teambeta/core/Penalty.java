78e623189502ea74bac31ab0cbfb4b4afc6b5bd1
David Li <dla133@sfu.ca>
2016-06-01 22:59:48 -0700

    Add Penalty.java for penalty enum

---
+++ src/ca/sfu/teambeta/core/Penalty.java
@@ -0,0 +1,24 @@
+package ca.sfu.teambeta.core;
+
+/**
+ * Created by constantin on 27/05/16.
+ * <p>
+ * <p>
+ * USAGE: After all of the games took place
+ * (1) pass groups to LadderManager
+ * (2) call processLadder() for all the computations to be complete.
+ */
+
+public enum Penalty {
+    ZERO(0), ABSENT(2), LATE(4), MISSING(10);
+
+    private int penalty;
+
+    Penalty(int penalty) {
+        this.penalty = penalty;
+    }
+
+    public int getPenalty() {
+        return penalty;
+    }
+}
