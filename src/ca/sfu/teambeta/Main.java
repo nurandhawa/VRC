b5bdb952143f26ed7e5c3c7975320a7fa122b2c4
nra22 <nra22@sfu.ca>
2016-06-02 20:46:19 -0700

    Finalised GameManager. Changed from Players to Pairs and deleted all printing statements.

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,5 +1,7 @@
 package ca.sfu.teambeta;
 
+import ca.sfu.teambeta.logic.GameManager;
+
 class Main {
     public static void main(String args[]) {
 
--- src/ca/sfu/teambeta/logic/GameManager.java
+++ src/ca/sfu/teambeta/logic/GameManager.java
@@ -1,5 +1,6 @@
 package ca.sfu.teambeta.logic;
 
+import ca.sfu.teambeta.core.Pair;
 import ca.sfu.teambeta.core.Player;
 import ca.sfu.teambeta.core.Scorecard;
 import java.util.ArrayList;
@@ -11,91 +12,66 @@ import java.util.Random;
  */
 public class GameManager {
 
-    private List<Player> ladder;
-    private List<Scorecard<Player>> groups;
+    private List<Pair> ladder;
+    private List<Scorecard<Pair>> groups;
 
-    public GameManager() {
-        ladder = new ArrayList<>();
+    public GameManager(List<Pair> activeLadder) {
+        ladder = activeLadder;
         groups = new ArrayList<>();
 
-        /* will not need this once ladder is up and filled already.
-        * I am using a Player object and using "playerId" field to incorporate tags of will or will not play for the time being.*/
-        fillUpLadder();
         splitLadderIntoGroups();
-
-        for (Scorecard<Player> s : groups) {
-            String sample[][] = new String[][]{
-                    {"L", "W", "L", "W"},
-                    {"", "W", "L", ""},
-                    {"L", "", "W", ""}
-            };
-            inputMatchResults(s, sample);
-        }
     }
 
-    public List<Scorecard<Player>> getScorecards() {
+    public List<Scorecard<Pair>> getScorecards() {
         return groups;
     }
 
     private void splitLadderIntoGroups() {
-        int playingCount = getWillingToPlayCount();
-        System.out.println("Players willing to play: " + playingCount);
+        int playingCount = ladder.size();
 
-        ArrayList<Player> groupings = new ArrayList<>();
+        ArrayList<Pair> groupings = new ArrayList<>();
 
         if (playingCount % 3 == 0) {
-            System.out.println("All 3 team groups.");
+            //All 3 team groups.
             int noOfTripleGroups = playingCount / 3;
             makeTripleGroups(noOfTripleGroups, groupings);
 
         } else if (playingCount % 3 == 1) {
-            System.out.println("One 4 team group");
+            //One 4 team group.
             int noOftripleGroups = playingCount / 3 - 1;
             int currentIndex = makeTripleGroups(noOftripleGroups, groupings);
             makeQuadGroup(currentIndex, groupings);
         } else {
-            System.out.println("Two 4 team group");
+            //Two 4 team groups.
             int noOftripleGroups = playingCount / 3 - 2;
             int currentIndex = makeTripleGroups(noOftripleGroups, groupings);
             makeQuadGroup(currentIndex, groupings);
         }
     }
 
-    private void makeQuadGroup(int num, ArrayList<Player> groupings) {
+    private void makeQuadGroup(int num, ArrayList<Pair> groupings) {
         for (int i = num; i < ladder.size(); i++) {
-            if (ladder.get(i).getPlayerID() == 1) {
-                groupings.add(ladder.get(i));
-            }
+            groupings.add(ladder.get(i));
 
             if (groupings.size() == 4) {
-                Scorecard<Player> s = new Scorecard<>(groupings);
+                Scorecard<Pair> s = new Scorecard<>(groupings);
                 groups.add(s);
-
-                for (Player p : groupings) {
-                    System.out.printf("%5s", p.getName());
-                }
                 System.out.println();
                 groupings.clear();
             }
         }
     }
 
-    private int makeTripleGroups(int num, ArrayList<Player> groupings) {
+    private int makeTripleGroups(int num, ArrayList<Pair> groupings) {
         int doneGroups = 0;
         int indexPosition = 0;
 
         for (int i = 0; i < ladder.size(); i++) {
-            if (ladder.get(i).getPlayerID() == 1) {
-                groupings.add(ladder.get(i));
-            }
+            groupings.add(ladder.get(i));
 
             if (groupings.size() == 3) {
-                Scorecard<Player> s = new Scorecard<>(groupings);
+                Scorecard<Pair> s = new Scorecard<>(groupings);
                 groups.add(s);
-
-                for (Player p : groupings) {
-                    System.out.printf("%5s", p.getName());
-                }
                 System.out.println();
                 groupings.clear();
                 doneGroups++;
@@ -108,51 +84,17 @@ public class GameManager {
         return indexPosition;
     }
 
-
-    private int getWillingToPlayCount() {
-        int count = 0;
-
-        for (Player p : ladder) {
-            if (p.getPlayerID() == 1) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private void fillUpLadder() {
-        Random rand = new Random();
-
-        for (int i = 0; i < 25; i++) {
-            int n = rand.nextInt(2);
-            String name = String.valueOf(Character.toChars(65 + i));
-            Player p = new Player(n, name);
-            ladder.add(p);
-        }
-        displayLadder();
-    }
-
-    private void displayLadder() {
-        System.out.printf("%10s %15s", "TEAMS", "   WILLING TO PLAY");
-        System.out.println();
-
-        for (int i = 0; i < ladder.size(); i++) {
-            System.out.printf("%10s %10d", ladder.get(i).getName(), ladder.get(i).getPlayerID());
-            System.out.println();
-        }
-    }
-
-    public void inputMatchResults(Scorecard<Player> s, String results[][]) {
-        List<Player> players = s.getTeamRankings();
+    public void inputMatchResults(Scorecard<Pair> s, String results[][]) {
+        List<Pair> teams = s.getTeamRankings();
         int rows = results.length;
-        int cols = players.size();
+        int cols = teams.size();
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (results[i][j].equals("W")) {
-                    s.setWin(players.get(j), i);
+                    s.setWin(teams.get(j), i);
                 } else if (results[i][j].equals("L")) {
-                    s.setLose(players.get(j), i);
+                    s.setLose(teams.get(j), i);
                 } else {
                 }
             }
