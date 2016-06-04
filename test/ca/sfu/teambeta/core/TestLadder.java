a020f2bb30e7090fa025ef0509fb1f2930d0ef92
Samuel Kim <ssk20@sfu.ca>
2016-06-04 13:52:11 -0700

    Included asserts for more comprehensive testing

--- test/ca/sfu/teambeta/core/TestLadder.java
+++ test/ca/sfu/teambeta/core/TestLadder.java
@@ -1,6 +1,7 @@
 package ca.sfu.teambeta.core;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,45 +19,63 @@ public class TestLadder {
             Ladder testLadder = new Ladder(someList);
 
         System.out.println("Adding pair at end Hughes/Mayes");
-        testLadder.insertAtEnd(new Pair(new Player(0, "Hughes"), new Player( 1, "Mayes")));
+        Pair thisPair = new Pair(new Player(0, "Hughes"), new Player( 1, "Mayes"));
+        testLadder.insertAtEnd(thisPair);
+        Assert.assertEquals(testLadder.getPairAtIndex(0), thisPair);
         System.out.println("Adding pair at end Joan/Mario");
-        testLadder.insertAtEnd(new Pair(new Player(2, "Joan"), new Player( 3, "Mario")));
+        thisPair = new Pair(new Player(2, "Joan"), new Player( 3, "Mario"));
+        testLadder.insertAtEnd(thisPair);
+        Assert.assertEquals(testLadder.getPairAtIndex(1), thisPair);
         System.out.println("Adding pair at end Luke/Heather");
-        testLadder.insertAtEnd(new Pair(new Player(4, "Luke"), new Player( 5, "Heather")));
+        thisPair = new Pair(new Player(4, "Luke"), new Player( 5, "Heather"));
+        testLadder.insertAtEnd(thisPair);
+        Assert.assertEquals(testLadder.getPairAtIndex(2), thisPair);
         System.out.println("Adding pair at index 1 Jack/Ethan");
-        testLadder.insertAtIndex(1, new Pair(new Player(6, "Jack"), new Player(7, "Ethan")));
+        thisPair = new Pair(new Player(6, "Jack"), new Player(7, "Ethan"));
+        testLadder.insertAtIndex(1, thisPair);
+        Assert.assertEquals(testLadder.getPairAtIndex(1), thisPair);
+        Assert.assertEquals(testLadder.getLadderLength(), 4);
 
         System.out.println();
-        for(Pair thisPair:testLadder.getLadder()){
-            System.out.println(thisPair.toString());
+        for(Pair iterPair:testLadder.getLadder()){
+            System.out.println(iterPair.toString());
         }
         System.out.println();
         System.out.println("Testing Remove:");
 
         System.out.println("Removing pair at index 0 (position 1)");
         Pair pairToRemove = testLadder.getLadder().get(0);
+        thisPair = testLadder.getLadder().get(1);
         testLadder.removePair(pairToRemove);
+        Assert.assertEquals(thisPair, testLadder.getLadder().get(0));
+        Assert.assertEquals(testLadder.getLadderLength(), 3);
 
-        for(Pair thisPair:testLadder.getLadder()){
-            System.out.println(thisPair.toString());
+        for(Pair iterPair:testLadder.getLadder()){
+            System.out.println(iterPair.toString());
         }
         System.out.println();
 
         System.out.println("Removing pair at index 1 (position 2)");
         pairToRemove = testLadder.getLadder().get(1);
+        thisPair = testLadder.getLadder().get(2);
         testLadder.removePair(pairToRemove);
+        Assert.assertEquals(thisPair, testLadder.getLadder().get(1));
+        Assert.assertEquals(testLadder.getLadderLength(), 2);
 
-        for(Pair thisPair:testLadder.getLadder()){
-            System.out.println(thisPair.toString());
+        for(Pair iterPair:testLadder.getLadder()){
+            System.out.println(iterPair.toString());
         }
         System.out.println();
 
         System.out.println("Removing pair at index 0 (position 1)");
         pairToRemove = testLadder.getLadder().get(0);
+        thisPair = testLadder.getLadder().get(1);
         testLadder.removePair(pairToRemove);
+        Assert.assertEquals(thisPair, testLadder.getLadder().get(0));
+        Assert.assertEquals(testLadder.getLadderLength(), 1);
 
-        for(Pair thisPair:testLadder.getLadder()){
-            System.out.println(thisPair.toString());
+        for(Pair iterPair:testLadder.getLadder()){
+            System.out.println(iterPair.toString());
         }
         System.out.println();
     }
