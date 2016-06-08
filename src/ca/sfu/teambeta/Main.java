1fc41aea0ca1d6ae928f4986b70736cb3c8c933a
Gordon Shieh <gshieh@sfu.ca>
2016-06-08 16:33:36 -0700

    Add Prototyping for Spark and JSON

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -6,16 +6,16 @@ import ca.sfu.teambeta.logic.GameManager;
 import ca.sfu.teambeta.logic.LadderManager;
 import ca.sfu.teambeta.ui.UserInterface;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
 
-//        Ladder loadedLadder = DBManager.loadFromDB();
-//        LadderManager ladderManager = new LadderManager(loadedLadder.getLadder());
-//
-//        ladderManager.getLadder().forEach(ladderManager::setIsPlaying);
-//
-//        GameManager gameManager = new GameManager(ladderManager.getActivePairs(), ladderManager);
-//        UserInterface.start(gameManager, ladderManager);
+        Ladder loadedLadder = DBManager.loadFromDB();
+        LadderManager ladderManager = new LadderManager(loadedLadder.getLadder());
+
+        ladderManager.getLadder().forEach(ladderManager::setIsPlaying);
+
+        GameManager gameManager = new GameManager(ladderManager.getActivePairs(), ladderManager);
+        UserInterface.start(gameManager, ladderManager);
 
     }
 }
