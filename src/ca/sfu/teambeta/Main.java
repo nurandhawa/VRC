ae2ddc418e58c6279c9586e81d122a3438e0419c
Gordon Shieh <gshieh@sfu.ca>
2016-06-14 21:55:09 -0700

    Modify to host static files from web root directory.

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,20 +1,15 @@
 package ca.sfu.teambeta;
 
-import com.google.gson.Gson;
-
-import ca.sfu.teambeta.core.Pair;
-import ca.sfu.teambeta.core.Player;
-
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.staticFiles;
 
 public class Main {
-    private static Gson gson = new Gson();
     public static void main(String[] args) {
         port(8000);
+        staticFiles.location(".");
         get("/", (request, response) -> {
-            response.type("application/json");
-            return gson.toJson(new Pair(new Player(1, "Bob"), new Player(2, "Bobby")));
+            return "";
         });
     }
 }
