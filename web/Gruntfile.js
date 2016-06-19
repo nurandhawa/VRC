1041808800aedab7a2ee25532c0a33b0a42f3802
Alex Land <aland@sfu.ca>
2016-06-19 15:54:17 -0700

    Set up Jasmine testing for API library
    Modify the Gruntfile to add a Jasmine task that runs tests for the API
    library. Moved 3rd party JS libraries into new vendor directory for
    ease of configuration and better organization. This test relies on
    api.js, a file that doesn't exist yet.

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -3,9 +3,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         jshint: {
             all: ['js/*.js']
+        },
+        jasmine: {
+          api: {
+            src: ['js/api.js'],
+            options: {
+              specs: 'test/APITest.js',
+              vendor: ['vendors/jquery-1.12.4.js']
+            }
+          }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
 };
