3e8fa9edfd126f3ac83bc76863fd35c66d0cc703
Samuel Kim <ssk20@sfu.ca>
2016-06-18 20:37:51 -0700

    Implement full dynamic matchcard creation
    This commit completes the dynamic matchcard creation feature-- each card (with the exception of an "other" card created for containing David's popup functionality) is created on the fly by parsing through the JSON object which holds all match information. Other changes include defaulting the border to be blue instead of green (panel-info instead of panel-success).

--- web/js/app.js
+++ web/js/app.js
@@ -33,11 +33,45 @@ var matchData = [
             },
             {
                         matchRanking: 2,
-                        name: "Alex Land and David Li",
+                        name: "Floyd Mayweather and Manny Pacquiao",
             },
             {
                         matchRanking: 3,
-                        name: "Alex Land and David Li",
+                        name: "Clark Kent and Bruce Wayne",
+            }
+        ]
+    },
+    {
+        matchNum: 2,
+        pairs: [
+            {
+                        matchRanking: 1,
+                        name: "Gordon Shieh and Jas Jassal",
+            },
+            {
+                        matchRanking: 2,
+                        name: "Tony Stark and Steve Rogers",
+            },
+            {
+                        matchRanking: 3,
+                        name: "Bruce Banner and Natascha Romanoff",
+            }
+        ]
+    },
+    {
+        matchNum: 3,
+        pairs: [
+            {
+                        matchRanking: 1,
+                        name: "Noor Randhawa and Constantin Koval",
+            },
+            {
+                        matchRanking: 2,
+                        name: "Jason Statham and Liam Neeson",
+            },
+            {
+                        matchRanking: 3,
+                        name: "Scott Summers and Peter Parker",
             }
         ]
     },
@@ -46,35 +80,60 @@ var matchData = [
         pairs: [
             {
                         matchRanking: 1,
-                        name: "Alex Land and David Li",
+                        name: "Raymond Huang and Samuel Kim",
             },
             {
                         matchRanking: 2,
-                        name: "Alex Land and David Li",
+                        name: "Oda Nobunaga and Tokugawa Ieyasu",
             },
             {
                         matchRanking: 3,
-                        name: "Alex Land and David Li",
+                        name: "Enrico Dandolo and Suleiman III",
             }
         ]
-    }
+    },
     {
-            matchNum: 3,
-            pairs: [
-                {
-                            matchRanking: 1,
-                            name: "Alex Land and David Li",
-                },
-                {
-                            matchRanking: 2,
-                            name: "Alex Land and David Li",
-                },
-                {
-                            matchRanking: 3,
-                            name: "Alex Land and David Li",
-                }
-            ]
-        }
+        matchNum: 5,
+        pairs: [
+            {
+                        matchRanking: 1,
+                        name: "Matt Murdock and Luke Cage",
+            },
+            {
+                        matchRanking: 2,
+                        name: "James Bond and Jason Bourne",
+            },
+            {
+                        matchRanking: 3,
+                        name: "Lisa Simpson and Bart Simpson",
+            },
+            {
+                        matchRanking: 4,
+                        name: "Edward Elric and Alphonse Elric",
+            }
+        ]
+    },
+    {
+        matchNum: 6,
+        pairs: [
+             {
+                        matchRanking: 1,
+                        name: "Harry Potter and Ron Weasley",
+             },
+             {
+                        matchRanking: 2,
+                        name: "Jon Snow and Ygritte Wildling",
+             },
+             {
+                        matchRanking: 3,
+                        name: "Khal Drogo and Daenerys Targaryen",
+             },
+             {
+                        matchRanking: 4,
+                        name: "Roy Mustang and Mayes Hughes",
+             }
+        ]
+    }
 ];
 
 var editFunction = function() {
