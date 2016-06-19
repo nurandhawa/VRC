d7cd24cbf1cac4860d2dc4e58cd1f8462a7947a2
Gordon Shieh <gshieh@sfu.ca>
2016-06-19 01:59:40 -0700

    Add test for adding new Player into the database

---
+++ test/ca/sfu/teambeta/persistence/PersistenceTest.java
@@ -0,0 +1,20 @@
+package ca.sfu.teambeta.persistence;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+
+
+public class PersistenceTest {
+    Session session;
+
+    @Before
+    public void setupDatabase() {
+        SessionFactory factory = DBManager.getMySQLSession();
+        this.session = factory.openSession();
+    }
+
+    protected Session getSession() {
+        return session;
+    }
+}
--- test/ca/sfu/teambeta/persistence/PlayerDBTest.java
+++ test/ca/sfu/teambeta/persistence/PlayerDBTest.java
@@ -1,6 +1,48 @@
 package ca.sfu.teambeta.persistence;
 
 
-public class PlayerDBTest {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import ca.sfu.teambeta.core.Player;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class PlayerDBTest extends PersistenceTest {
+
+    @Test
+    public void testPlayerCreation() {
+        Transaction tx;
+        Session session = getSession();
+        tx = session.beginTransaction();
+        session.save(new Player("Big", "Head", ""));
+        tx.commit();
+    }
+
+    @Test
+    public void testUniquePlayerID() {
+        Transaction tx;
+        Session session = getSession();
+        tx = session.beginTransaction();
+        int id1 = (int) session.save(new Player("Big", "Head", ""));
+        int id2 = (int) session.save(new Player("Big", "Head", ""));
+        tx.commit();
+        assertNotEquals(id1, id2);
+    }
+
+    @Test
+    public void testSamePlayerID() {
+        Transaction tx;
+        Session session = getSession();
+        tx = session.beginTransaction();
+        Player player = new Player("Big", "Head", "");
+        int id1 = (int) session.save(player);
+        int id2 = (int) session.save(player);
+        tx.commit();
+        assertEquals(id1, id2);
+    }
+
 
 }
