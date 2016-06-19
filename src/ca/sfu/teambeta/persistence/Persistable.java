9713a4aed11728e193b72c45b745881d0370d2b6
Gordon Shieh <gshieh@sfu.ca>
2016-06-19 01:32:53 -0700

    Reimplement equals and hashCode for Player, Pair, and Ladder
    No longer using id field to uniquely identify objects

--- src/ca/sfu/teambeta/persistence/Persistable.java
+++ src/ca/sfu/teambeta/persistence/Persistable.java
@@ -15,27 +15,9 @@ public abstract class Persistable {
         return id;
     }
 
-    // Keep this setter for the purpose of comparing IDs during tests
-    protected void setID(int id) {
-        this.id = id;
-    }
+    @Override
+    public abstract boolean equals(Object o);
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Persistable object = (Persistable) o;
-        return id == object.id;
-    }
-
-    @Override
-    // Once we get a database setup, the id attribute will be guaranteed unique
-    public int hashCode() {
-        return id;
-    }
+    public abstract int hashCode();
 }
