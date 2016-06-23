95fe44af7b0923ed3fd8b0def41447085e83ebac
Gordon Shieh <gshieh@sfu.ca>
2016-06-22 19:11:20 -0700

    Add Implementation for new Scorecard and Game object
    New implementation is database friendly

---
+++ src/ca/sfu/teambeta/core/Game.java
@@ -0,0 +1,62 @@
+package ca.sfu.teambeta.core;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import ca.sfu.teambeta.persistence.Persistable;
+
+@Entity(name = "Game")
+public class Game extends Persistable {
+
+    @OneToOne
+    private Pair winner;
+
+    @OneToOne
+    private Pair loser;
+
+    public Game() {
+    }
+
+    public Game(Pair winner, Pair loser) {
+        this.winner = winner;
+        this.loser = loser;
+    }
+
+    public boolean isWinner(Pair pair) {
+        return winner.equals(pair);
+    }
+
+    public boolean isLoser(Pair pair) {
+        return loser.equals(pair);
+    }
+
+    public Pair getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Pair winner) {
+        this.winner = winner;
+    }
+
+    public Pair getLoser() {
+        return loser;
+    }
+
+    public void setLoser(Pair loser) {
+        this.loser = loser;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        final Game otherGame = (Game) other;
+        return getWinner().equals(otherGame.getWinner()) && getLoser().equals(otherGame.getLoser());
+    }
+
+    @Override
+    public int hashCode() {
+        return 37 * getWinner().hashCode() * getLoser().hashCode();
+    }
+}
--- src/ca/sfu/teambeta/core/Scorecard.java
+++ src/ca/sfu/teambeta/core/Scorecard.java
@@ -1,117 +1,88 @@
 package ca.sfu.teambeta.core;
 
-import com.google.gson.annotations.Expose;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-/**
- * Created by Gordon Shieh on 25/05/16.
- */
-public class Scorecard<T> {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import ca.sfu.teambeta.persistence.Persistable;
+
+
+@Entity(name = "Scorecard")
+public class Scorecard extends Persistable {
     private static final int WIN = 1;
     private static final int NO_SCORE = 0;
     private static final int LOSE = -1;
     private static final int NUM_GAMES = 4;
 
-    @Expose
-    private Map<T, List<Integer>> scoreMap;
-    private Observer observer = null;
-    private int setCount = 0;
+    @OneToMany
+    Set<Game> games = new HashSet<>();
 
-    public Scorecard(List<T> teams, Observer obs) {
-        int numTeams = teams.size();
-        scoreMap = new LinkedHashMap<>(numTeams);
-        for (T t : teams) {
-            List<Integer> emptyScores = new ArrayList<>(Collections.nCopies(NUM_GAMES, NO_SCORE));
-            scoreMap.put(t, emptyScores);
-        }
-        observer = obs;
+    List<Pair> pairs = new ArrayList<>();
+
+    @Transient
+    Observer observer = null;
+    int finishedGameCount = 0;
+
+    public Scorecard(List<Pair> pairs, Observer obs) {
+        this.pairs = pairs;
+        this.observer = obs;
     }
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Canucks", "Flames", "Oilers", "Leafs");
-        Scorecard<String> sc = new Scorecard<>(list, null);
+        Pair p1 = new Pair(new Player("A", "A", ""), new Player("B", "B", ""));
+        Pair p2 = new Pair(new Player("C", "C", ""), new Player("D", "D", ""));
+        Pair p3 = new Pair(new Player("E", "E", ""), new Player("F", "F", ""));
+        List<Pair> pairs = Arrays.asList(p1, p2, p3);
+        Scorecard sc = new Scorecard(pairs, null);
 
-        sc.setWin("Canucks", 0);
-        sc.setWin("Oilers", 1);
-        sc.setWin("Canucks", 2);
-        System.out.println(sc.getScore("Canucks"));
-        System.out.println(sc.getTeamRankings().toString());
+        sc.setGameResults(p2, p1);
+        sc.setGameResults(p3, p2);
+        sc.setGameResults(p3, p1);
+
+        for (Pair p : sc.getReorderedPairs()) {
+            System.out.println(p.toString());
+        }
     }
 
-    private void setStatus(T team, int matchNum, int status) {
-        assert (status != NO_SCORE);
-        List<Integer> scoreList = scoreMap.get(team);
-
-        // Should only track newly set scores,
-        // a score update should not count towards the number of sets
-        if (scoreList.get(matchNum) == NO_SCORE) {
-            setCount++;
-        }
-        scoreList.set(matchNum, status);
-
-        if (observer != null && isFilled()) {
+    public void setGameResults(Pair winner, Pair loser) {
+        games.add(new Game(winner, loser));
+        finishedGameCount++;
+        if (observer != null && finishedGameCount == pairs.size()) {
             observer.done();
         }
     }
 
-    private boolean isFilled() {
-        int numTeams = scoreMap.keySet().size();
-        return setCount == (2 * numTeams);
+    private int getPairScore(Pair pair) {
+        int winCount = (int) games.stream()
+                .filter(game -> game.isWinner(pair))
+                .count();
+
+        int loseCount = (int) games.stream()
+                .filter(game -> game.isLoser(pair))
+                .count();
+        return winCount - loseCount;
     }
 
-    public void setWin(T team, int matchNum) {
-        setStatus(team, matchNum, WIN);
-    }
-
-    public void setLose(T team, int matchNum) {
-        setStatus(team, matchNum, LOSE);
-    }
-
-    public void unsetStatus(T team, int matchNum) {
-        List<Integer> scoreList = scoreMap.get(team);
-
-        if (scoreList.get(matchNum) != NO_SCORE) {
-            setCount--;
-        }
-        scoreList.set(matchNum, NO_SCORE);
-    }
-
-    public int getScore(T team) {
-        return scoreMap.get(team).stream()
-                .mapToInt(Integer::intValue).sum();
-    }
-
-    // Java uses a stable sorting algorithm (TimSort) so that if there are ties,
-    // the original order does not change
-    public List<T> getTeamRankings() {
-        List<T> teams = new ArrayList<>(scoreMap.keySet());
-        teams.sort((T a, T b) -> getScore(b) - getScore(a));
-        return teams;
+    public List<Pair> getReorderedPairs() {
+        List<Pair> orderedPairs = new ArrayList<>(pairs);
+        Collections.sort(orderedPairs, (pair1, pair2) -> getPairScore(pair2) - getPairScore(pair1));
+        return orderedPairs;
     }
 
     @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (Map.Entry<T, List<Integer>> entry : scoreMap.entrySet()) {
-            builder.append(entry.getKey().toString());
-            builder.append("\t");
-            for (Integer i : entry.getValue()) {
-                if (i == WIN) {
-                    builder.append("Win\t");
-                } else if (i == LOSE) {
-                    builder.append("Lost\t");
-                } else {
-                    builder.append("Pending\t");
-                }
-            }
-            builder.append("\n");
-        }
-        return builder.toString();
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
     }
 }
