176917ed3659bc6ceb91d88854cbd87dfd9bfebb
jasdeep-jassal <jjassal@sfu.ca>
2016-06-26 23:41:01 -0700

    Add sessions to save user login state
    Added notion of a user session so that the front end/back end does
    not have to authenticate the user everytime a page is loaded, instead
    a session token can be passed in.

--- src/ca/sfu/teambeta/logic/UserSessionManager.java
+++ src/ca/sfu/teambeta/logic/UserSessionManager.java
@@ -1,29 +1,117 @@
 package ca.sfu.teambeta.logic;
 
+import ca.sfu.teambeta.core.Session;
 import ca.sfu.teambeta.core.User;
+import ca.sfu.teambeta.core.exceptions.NoSuchSessionException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * UserSessionManager handles
+ * UserSessionManager handles:
  * - Creating a new session
+ * - Authenticating an existing session token
  *
- * Coming Soon
+ *
+ * Coming Soon:
  * - Backing session's up to database
+ *
+ *
+ * TODO:
+ * - Perhaps have AccountManager hold a UserSessionManager object for increased security
+ * - Have a UDID as the key to our 'sessions' dictionary so that the user may login on multiple devices
+ *
  */
+
 public class UserSessionManager {
-    private static List<String> sessions = new ArrayList<>();
+    private static Dictionary<String, Session> sessions = new Hashtable<>();
+    private static final String DEMO_SESSION_ID = "T3STS355IONID";
 
 
-    // MARK: - The Core Create/Delete Session Methods
+    // MARK: - The Core Session Methods
     public static String createNewSession(User user) {
-        String sessionID = "DI4J59JEN2XC39XFJJ30ASD3";
-        sessions.add(sessionID);
+        // Create a session, if a session with the email already exists, it will be overridden
+        // TODO: Randomly generate a token
+        String sessionID = DEMO_SESSION_ID;
+        String expiryDate = "datePlaceholder";
+
+        Session userSession = new Session(sessionID, expiryDate);
+
+        String userEmail = user.getEmail();
+
+        sessions.put(userEmail, userSession);
 
         return sessionID;
 
     }
 
+    public static void deleteSession(String email, String token) throws NoSuchSessionException {
+        Session session = getUserSession(email);
+
+        sessions.remove(email);
+    }
+
+    public static boolean authenticateToken(String email, String token) throws NoSuchSessionException {
+        Session session = getUserSession(email);
+
+        // TODO: Check expiry date, beef up security
+        if (session.getToken().equals(token)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    // MARK: Helper Methods
+    private static Session getUserSession(String email) throws NoSuchSessionException {
+        Session session = sessions.get(email);
+
+        if (session == null) {
+            throw new NoSuchSessionException("No session exists for user: " + email);
+        } else {
+            return session;
+        }
+    }
+
+
+    // MARK: Main Function (Quick and dirty testing for now - will be refactored into tests)
+    public static void main(String[] args) {
+        // Creating a session
+        User testUser1 = new User("admin@vrc.ca", "pwHash1");
+        User testUser2 = new User("john@vrc.ca", "pwHash2");
+
+        createNewSession(testUser1);
+        createNewSession(testUser1); // Will update previous entry
+        createNewSession(testUser2);
+
+        System.out.println(sessions.get("admin@vrc.ca").getToken());
+        System.out.println("Number of session: " + sessions.size()); // Will be 2
+
+
+        // Authenticating a session
+        boolean tokenStatus;
+        try {
+            tokenStatus = authenticateToken("admin@vrc.ca", DEMO_SESSION_ID);
+        } catch (NoSuchSessionException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        System.out.println("Session exists?: " + tokenStatus);
+
+
+        // Deleting a session
+        try {
+            deleteSession("admin@vrc.ca", DEMO_SESSION_ID);
+        } catch (NoSuchSessionException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        System.out.println("Number of session: " + sessions.size()); // Will be 1
+
+
+    }
 
 }
