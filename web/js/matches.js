e24114b2e215bbc565a644af7db9113101b4d823
David Li <dla133@sfu.ca>
2016-06-27 23:15:00 -0700

    Fix wrong team information on modal component

--- web/js/matches.js
+++ web/js/matches.js
@@ -2,11 +2,11 @@
  * Created by David on 2016-06-15.
  */
 
-var EDIT_BUTTON_HTML = '<a v-on:click="modalOpen($index)" class="edit-button btn btn-info btn-fab btn-fab-mini"><i class="material-icons md-light">create</i></a>';
+var EDIT_BUTTON_HTML = '<a v-on:click="editMatch()" class="edit-button btn btn-info btn-fab btn-fab-mini"><i class="material-icons md-light">create</i></a>';
 
-Vue.filter('filterLeft', function(array, matchNum) {
+Vue.filter('filterLeft', function (array, matchNum) {
     var filteredArray = [];
-    if(matchNum % 3 == 1){
+    if (matchNum % 3 == 1) {
         filteredArray.add(array[matchNum])
     }
 });
@@ -24,7 +24,7 @@ Vue.component('matches', {
         }
     },
     filters: {
-        filterLeft: function(matchNum){
+        filterLeft: function (matchNum) {
             var matches = [];
             matches.add(1);
             matches.add(2);
@@ -33,10 +33,10 @@ Vue.component('matches', {
         }
     },
     methods: {
-        modalActiveContent: function(i) {
+        modalActiveContent: function (i) {
             return this.active === i;
         },
-        setModalClose: function() {
+        setModalClose: function () {
             this.show = false;
             this.active = false;
 
@@ -44,34 +44,24 @@ Vue.component('matches', {
     }
 });
 
-var Matches = (function() {
+var Matches = (function () {
     function Matches(matchData) {
-/*        for (var match in matchData){
+        for (var match in matchData){
             var editButton = Vue.extend({
-                 props: ['column','index'],
-                 template: EDIT_BUTTON_HTML,
-                 methods: {
-                     modalOpen: function() {
-                        if (this.column === "left") {
-                            var modal = $("#matches-left,#modal" + this.index)[1];
-                            modal.modal("show");
-                        }
-                        else if (column === "mid") {
-
-                        }
-                        else {
-
-                        }
+                props: ['column','index'],
+                template: EDIT_BUTTON_HTML,
+                methods: {
+                    editMatch: function() {
                         this.$parent.modalOpen(this.index);
                     }
                  }
-             });
-             Vue.component('edit-button', editButton);
+            });
+            Vue.component('edit-button', editButton);
 
-             var blankButton = Vue.extend({
+            var blankButton = Vue.extend({
                 template: "<a></a>"
             });
-        } */
+        }
 
         this.component = new Vue({
             el: '#matches',
@@ -82,28 +72,23 @@ var Matches = (function() {
                 mode: 'read'
             },
             methods: {
-                filterLeft: function() {
+                filterLeft: function () {
                     return 1;
                 },
-                modalOpen: function(i) {
+                modalOpen: function (i) {
                     this.showModal = true;
                     this.active = i;
                     return this.active;
                 }
-//                 modalOpen2: function(i, name) {
-//                    this.showModal = true;
-//                    this.$refs[name].show = true;
-//                    return this.active = i;
-//                }
             },
-    /*        components: {
-                edit: editButton,
-                read: blankButton
-            }
-        })
+            components: {
+             edit: editButton,
+             read: blankButton
+             }
+        });
     };
 
-    Matches.prototype.changeMode = function() {
+    Matches.prototype.changeMode = function () {
         if (this.mode === 'read') {
             this.mode = 'edit';
         }
