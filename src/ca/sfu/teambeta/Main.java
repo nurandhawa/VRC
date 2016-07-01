1f89349fb2cad99476f2702db120cefeab077279
nra22 <nra22@sfu.ca>
2016-06-30 21:57:14 -0700

    Testing from main. Comment it out

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,17 +1,27 @@
 package ca.sfu.teambeta;
 
+import ca.sfu.teambeta.core.Ladder;
+import ca.sfu.teambeta.core.Pair;
+import ca.sfu.teambeta.core.Player;
+import ca.sfu.teambeta.persistence.DBManager;
+import org.hibernate.SessionFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Main {
     public static void main(String[] args) {
 
         /*Laddgiter loadedLadder = DBManager.loadFromDB();*/
 
         /* -----FOR TESTING*/
- /*       List<Pair> ladderPairs = Arrays.asList(
-                new Pair(new Player("Bobby", "Chan", ""), new Player("Wing", "Man", ""), false),
-                new Pair(new Player("Ken", "Hazen", ""), new Player("Brian", "Fraser", ""), false),
-                new Pair(new Player("Simon", "Fraser", ""), new Player("Dwight", "Howard", ""), false),
-                new Pair(new Player("Bobby", "Chan", ""), new Player("Big", "Head", ""), false),
-                new Pair(new Player("Alex", "Land", ""), new Player("Test", "Player", ""), false)
+        /*List<Pair> ladderPairs = Arrays.asList(
+                new Pair(new Player("Bobby", "Chan"), new Player("Wing", "Man"), false),
+                new Pair(new Player("Ken", "Hazen"), new Player("Brian", "Fraser"), false),
+                new Pair(new Player("Simon", "Fraser"), new Player("Dwight", "Howard"), false),
+                new Pair(new Player("Bobby", "Chan"), new Player("Big", "Head"), false),
+                new Pair(new Player("Alex", "Land"), new Player("Test", "Player"), false)
         );
         Ladder newLadder = new Ladder(ladderPairs);
 
@@ -24,14 +34,8 @@ class Main {
         List<Pair> loadedLadderPairs = new ArrayList<>();
         loadedLadderPairs.addAll(loadedLadder.getPairs());
 
-        LadderManager ladderManager = new LadderManager(loadedLadderPairs);
+        AppController appController = new AppController(dbManager);*/
 
-        ladderManager.getLadder().forEach(ladderManager::setIsPlaying);
 
-        GameManager gameManager = new GameManager(ladderManager.getActivePairs(), ladderManager);
-        //UserInterface.start(gameManager,ladderManager);
-        AppController appController = new AppController(ladderManager, gameManager);
-
-*/
     }
 }
