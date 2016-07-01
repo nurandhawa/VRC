dda2b2aeb0866b7a57cc72c741c472cd46adf723
David Li <dla133@sfu.ca>
2016-07-01 01:43:25 -0700

    Add api call on matchloader.js and modify dropdowns on modal component

--- web/js/matchloader.js
+++ web/js/matchloader.js
@@ -6,120 +6,7 @@
 
     $.material.init();
 
-    var matchData = [
-        {
-            matchNum: 1,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Alex Land and David Li",
-                },
-                {
-                    matchRanking: 2,
-                    name: "Floyd Mayweather and Manny Pacquiao",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Clark Kent and Bruce Wayne",
-                }
-            ]
-        },
-        {
-            matchNum: 2,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Gordon Shieh and Jas Jassal",
-                },
-                {
-                    matchRanking: 2,
-                    name: "Tony Stark and Steve Rogers",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Bruce Banner and Natascha Romanoff",
-                }
-            ]
-        },
-        {
-            matchNum: 3,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Noor Randhawa and Constantin Koval",
-                },
-                {
-                    matchRanking: 2,
-                    name: "Jason Statham and Liam Neeson",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Scott Summers and Peter Parker",
-                }
-            ]
-        },
-        {
-            matchNum: 4,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Raymond Huang and Samuel Kim",
-                },
-                {
-                    matchRanking: 2,
-                    name: "Oda Nobunaga and Tokugawa Ieyasu",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Enrico Dandolo and Suleiman III",
-                }
-            ]
-        },
-        {
-            matchNum: 5,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Matt Murdock and Luke Cage",
-                },
-                {
-                    matchRanking: 2,
-                    name: "James Bond and Jason Bourne",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Lisa Simpson and Bart Simpson",
-                },
-                {
-                    matchRanking: 4,
-                    name: "Edward Elric and Alphonse Elric",
-                }
-            ]
-        },
-        {
-            matchNum: 6,
-            pairs: [
-                {
-                    matchRanking: 1,
-                    name: "Harry Potter and Ron Weasley",
-                },
-                {
-                    matchRanking: 2,
-                    name: "Jon Snow and Ygritte Wildling",
-                },
-                {
-                    matchRanking: 3,
-                    name: "Khal Drogo and Daenerys Targaryen",
-                },
-                {
-                    matchRanking: 4,
-                    name: "Roy Mustang and Mayes Hughes",
-                }
-            ]
-        }
-    ];
-
-    var matchData2 =  [
+    var matchData =  [
         {
             "pairs": [
                 {
@@ -380,12 +267,15 @@
         }
     ];
 
-    var matches = new Matches(matchData2);
+    // var api = new API();
+    // api.getMatches(function (matchData) {
+        var matches = new Matches(matchData);
 
-    var editFunction = function() {
-        matches.changeMode.call(matches.component);
-    };
+        var editFunction = function() {
+            matches.changeMode.call(matches.component);
+        };
 
-    var header = new Header("Matches", "Edit Matches", "TBD", editFunction);
+        var header = new Header("Matches", "Edit Matches", "TBD", editFunction);
 
+   // });
 })();
