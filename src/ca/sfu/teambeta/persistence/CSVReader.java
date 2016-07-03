ce3ba929a6ce453768c84ee01d3639fec2a7eb79
Kostiantyn Koval <kkoval@sfu.ca>
2016-07-02 17:25:58 -0700

    Populate DB with players from CSV, Fixes #21

--- src/ca/sfu/teambeta/persistence/CSVReader.java
+++ src/ca/sfu/teambeta/persistence/CSVReader.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import ca.sfu.teambeta.core.Player;
+import org.hibernate.SessionFactory;
 
 /**
  * Created by constantin on 29/06/16.
@@ -14,12 +15,26 @@ import ca.sfu.teambeta.core.Player;
 public class CSVReader {
     private static final String DEFAULT_FILENAME = "ladder.csv";
 
+    public static void main(String[] args) throws Exception {
+        List<Player> playersFromVRC;
+        try {
+            playersFromVRC = getInformationAboutPlayers();
+        } catch (Exception exception) {
+            throw exception;
+        }
+
+        SessionFactory factory = DBManager.getMySQLSession(false);
+        DBManager db = new DBManager(factory);
+        for (Player player : playersFromVRC) {
+            db.addNewPlayer(player);
+        }
+    }
+
     public static List<Player> getInformationAboutPlayers() throws Exception {
-        List<Player> players;
+        List<Player> players = new ArrayList<>();
         try (com.opencsv.CSVReader reader = new com.opencsv.CSVReader(new FileReader(DEFAULT_FILENAME))) {
             List<String[]> entries = reader.readAll();
             Iterator<String[]> iterator = entries.iterator();
-            players = new ArrayList<>();
 
             String[] pairInfo;
             while (iterator.hasNext()) {
@@ -27,12 +42,11 @@ public class CSVReader {
 
                 String lastName = pairInfo[0];
                 String firstName = pairInfo[1];
-                String id = pairInfo[2];
+                //String id = pairInfo[2];
 
-                if (lastName.equals("*") && firstName.equals("*") && id.equals("*")) {
-                    Player player = new Player(lastName, firstName);
-                    players.add(player);
-                }
+                Player player = new Player(lastName, firstName);
+                players.add(player);
+
             }
             reader.close();
         } catch (IOException e) {
