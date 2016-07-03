5fffa77edc4ac85c619ac66ba4fcb059878a0b24
Gordon Shieh <gshieh@sfu.ca>
2016-07-03 12:22:47 -0700

    Add argument in Main class to specify to use the Production database

--- src/ca/sfu/teambeta/Main.java
+++ src/ca/sfu/teambeta/Main.java
@@ -1,46 +1,32 @@
 package ca.sfu.teambeta;
 
+import org.hibernate.SessionFactory;
+
 import ca.sfu.teambeta.core.Ladder;
-import ca.sfu.teambeta.core.Pair;
-import ca.sfu.teambeta.core.Player;
 import ca.sfu.teambeta.logic.GameSession;
 import ca.sfu.teambeta.persistence.CSVReader;
 import ca.sfu.teambeta.persistence.DBManager;
-import org.hibernate.SessionFactory;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        DBManager dbManager;
+        if (args.length > 0 && args[0].equals("production")) {
+            SessionFactory sessionFactory = DBManager.getProductionSession();
+            dbManager = new DBManager(sessionFactory);
+        } else {
+            Ladder newLadder = null;
+            try {
+                newLadder = CSVReader.setupLadder();
+            } catch (Exception e) {
+                System.out.println("INVALID CSV FILE");
+                throw e;
+            }
+            SessionFactory sessionFactory = DBManager.getMySQLSession(true);
 
-        /*Laddgiter loadedLadder = DBManager.loadFromDB();*/
-
-        /* -----FOR TESTING*/
-        /*List<Pair> ladderPairs = Arrays.asList(
-                new Pair(new Player("Bobby", "Chan"), new Player("Wing", "Man"), true),
-                new Pair(new Player("Ken", "Hazen"), new Player("Brian", "Fraser"), true),
-                new Pair(new Player("Simon", "Fraser"), new Player("Dwight", "Howard"), true),
-                new Pair(new Player("Bobby", "Chan"), new Player("Big", "Head"), true),
-                new Pair(new Player("Alex", "Land"), new Player("Test", "Player"), false),
-                new Pair(new Player("Manuel", "Neuer"), new Player("Gigi", "Buffon"), true),
-                new Pair(new Player("Mesut", "Ozil"), new Player("Gareth", "Bale"), true)
-        );
-        Ladder newLadder = new Ladder(ladderPairs);*/
-
-        Ladder newLadder = null;
-        try {
-            newLadder = CSVReader.setupLadder();
-        } catch (Exception e) {
-            System.out.println("INVALID CSV FILE");
+            GameSession gameSession = new GameSession(newLadder);
+            dbManager = new DBManager(sessionFactory);
+            dbManager.persistEntity(gameSession);
         }
-
-        SessionFactory sessionFactory = DBManager.getMySQLSession(true);
-        DBManager dbManager = new DBManager(sessionFactory);
-        GameSession gameSession = new GameSession(newLadder);
-        dbManager.persistEntity(gameSession);
-
         AppController appController = new AppController(dbManager);
     }
 }
