d27d031f4fac7aa23be9516262a4695f12a2bf2c
Kostiantyn Koval <kkoval@sfu.ca>
2016-07-15 01:35:21 -0700

    Add TimeSelection interface

---
+++ src/ca/sfu/teambeta/logic/TimeSelection.java
@@ -0,0 +1,22 @@
+package ca.sfu.teambeta.logic;
+
+import ca.sfu.teambeta.core.Ladder;
+import ca.sfu.teambeta.core.Pair;
+import ca.sfu.teambeta.core.Scorecard;
+import ca.sfu.teambeta.core.Time;
+
+import java.util.List;
+
+/**
+ * Created by constantin on 10/07/16.
+ */
+public interface TimeSelection {
+
+    int getAmountPairsByTime(List<Pair> pairs, Time time);
+
+    List<Pair> getPairsByTime(List<Pair> pairs,  Time time);
+
+    void distributePairs(List<Scorecard> scorecards);
+
+    void clearTimeSlots(Ladder nextWeekLadder);
+}
