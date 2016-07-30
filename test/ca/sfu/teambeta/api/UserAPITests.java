7f0030e841ae0db8729ae7fbd4abc2d5d6436104
Gordon Shieh <gshieh@sfu.ca>
2016-07-29 19:01:30 -0700

    Rename tests to be consistent with rest of tests

--- test/ca/sfu/teambeta/api/UserAPITests.java
+++ test/ca/sfu/teambeta/api/UserAPITests.java
@@ -13,7 +13,7 @@ import static junit.framework.TestCase.assertNotNull;
 
 public class UserAPITests extends APITest {
     @Test
-    public void loginTest() throws UnirestException {
+    public void testValidLogin() throws UnirestException {
         HttpResponse<JsonNode> jsonResponse = login(EMAIL, PASSWORD, REMEMBER_ME);
 
         assertEquals(200, jsonResponse.getStatus());
@@ -22,7 +22,6 @@ public class UserAPITests extends APITest {
     }
 
     @Test(expected = JSONException.class)
-    public void loginFailureTest() throws UnirestException {
         HttpResponse<JsonNode> jsonResponse = login(EMAIL, "silly_me", REMEMBER_ME);
         assertEquals(401, jsonResponse.getStatus());
         JsonNode node = jsonResponse.getBody();
