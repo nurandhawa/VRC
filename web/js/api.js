gordonshieh/vrc-project

Add loading spinners to matches on result input, and Save Results button

web/js/api.js
@@ -189,10 +189,24 @@ var API = (function() {
         });
     };
 
-    API.prototype.reorderLadder = function(gameSession) {
+    API.prototype.reorderLadder = function (gameSession, doneCallback, failCallback) {
         $.ajax({
             method: "POST",
             url: SERVER_URL + "/matches" + GAMESESSION_PARAM + gameSession
+        })
+        .done(function (response) {
+            if (doneCallback) {
+                doneCallback(response);
+            }
+        })
+        .fail(function (response) {
+            if (failCallback) {
+                failCallback(response);
+            }
+            else {
+                var responseBody = JSON.parse(response.responseText);
+                alert(responseBody.message);
+            }
         });
     };
 
@@ -205,6 +219,7 @@ var API = (function() {
             var matches = JSON.parse(response);
             matches.forEach(function(match, i) {
                 match.scorecardIndex = i;
+                match.mode = "read";
                 match.resultsValid = false;
                 match.results = [];
                 match.pairs.forEach(function(pair) {

web/js/matchloader.js
@@ -20,8 +20,17 @@
         template: '<a v-on:click="saveResults()" class="btn btn-raised btn-success header-button">Save Results</a>',
         methods: {
             saveResults: function() {
+                var header = this.$parent;
+                header.mode = "loading";
                 var api = new API();
-                api.reorderLadder(api.gameSession.LATEST);
+                var doneCallback = function() {
+                    header.mode = "edit";
+                };
+                var failCallback = function(response) {
+                    header.mode = "edit";
+                    alert(JSON.parse(response.responseText).responseBody);
+                };
+                api.reorderLadder(api.gameSession.LATEST, doneCallback.bind(this), failCallback.bind(this));
             }
         },
         parent: header.component

nurandhawa/VRC

web/js/api.js
@@ -48,6 +48,9 @@ var API = (function() {
 
                     pair.teamName = player1Name + " and " + player2Name;
                     pair.playingStatus = pair.isPlaying ? "playing" : "notplaying";
+
+                    pair.mode = "read";
+
                     ladderData.players.push({
                         label: player1Name,
                         id: player1.id
@@ -204,10 +207,24 @@ var API = (function() {
             });
     };
 
-    API.prototype.reorderLadder = function(gameSession) {
+    API.prototype.reorderLadder = function (gameSession, doneCallback, failCallback) {
         $.ajax({
             method: "POST",
             url: SERVER_URL + "/matches" + GAMESESSION_PARAM + gameSession
+        })
+        .done(function (response) {
+            if (doneCallback) {
+                doneCallback(response);
+            }
+        })
+        .fail(function (response) {
+            if (failCallback) {
+                failCallback(response);
+            }
+            else {
+                var responseBody = JSON.parse(response.responseText);
+                alert(responseBody.message);
+            }
         });
     };
 
@@ -394,7 +411,7 @@ var API = (function() {
                 }
             });
     };
-    
+
     return API;
 
 })();

web/js/matchloader.js
@@ -23,8 +23,17 @@
         },
         methods: {
             saveResults: function() {
+                var header = this.$parent;
+                header.mode = "loading";
                 var api = new API();
-                api.reorderLadder(matches.component.activeGameSession);
+                var doneCallback = function() {
+                    header.mode = "edit";
+                };
+                var failCallback = function(response) {
+                    header.mode = "edit";
+                    alert(JSON.parse(response.responseText).responseBody);
+                };
+                api.reorderLadder(matches.component.activeGameSession, doneCallback.bind(this), failCallback.bind(this));
             }
         }
     });