dd93c0a7c45fb09c111f03d643bf27ab1def288e
Samuel Kim <ssk20@sfu.ca>
2016-08-02 01:34:11 -0700

    re-add missing line

--- web/js/login.js
+++ web/js/login.js
@@ -13,6 +13,7 @@
 
     var onLoggedIn = function(response) {
         Cookies.set('sessionToken', response.sessionToken);
+        - Cookies.set('userRole', response.userRole);
         window.location.href = "/";
     };
 
