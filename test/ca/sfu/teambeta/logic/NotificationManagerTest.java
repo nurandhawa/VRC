a2b9878e698fa4b5dc0fe41b6b06f1b8646ba3c3
Alex Land <aland@sfu.ca>
2016-10-06 09:30:51 -0700

    Added sendEmail test (to be used manually for testing emails)

--- test/ca/sfu/teambeta/logic/NotificationManagerTest.java
+++ test/ca/sfu/teambeta/logic/NotificationManagerTest.java
@@ -3,10 +3,12 @@ package ca.sfu.teambeta.logic;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.Calendar;
+import java.util.List;
 
 import ca.sfu.teambeta.accounts.AccountDatabaseHandler;
 import ca.sfu.teambeta.accounts.AccountManager;
@@ -15,11 +17,12 @@ import ca.sfu.teambeta.core.Pair;
 import ca.sfu.teambeta.notifications.EmailNotifier;
 import ca.sfu.teambeta.notifications.NotificationManager;
 import ca.sfu.teambeta.notifications.Notifier;
+import ca.sfu.teambeta.notifications.SimpleComposer;
 import ca.sfu.teambeta.persistence.CSVReader;
 import ca.sfu.teambeta.persistence.DBManager;
 
 /**
- * Created by AlexLand on 2016-10-02.
+ * Tests for the NotificationManager class
  */
 public class NotificationManagerTest {
     private DBManager dbManager;
@@ -32,14 +35,23 @@ public class NotificationManagerTest {
 
         GameSession gameSession = setupGameSession();
 
-        Pair activePair = gameSession.getAllPairs().get(0);
-        dbManager.setPairActive(dbManager.getGameSessionLatest(), activePair.getID());
+        List<Pair> activePairs = gameSession.getAllPairs();
+        Pair activePair1 = activePairs.get(0);
+        Pair activePair2 = activePairs.get(1);
+        Pair activePair3 = activePairs.get(2);
+        Pair activePair4 = activePairs.get(3);
+        dbManager.setPairActive(dbManager.getGameSessionLatest(), activePair1.getID());
+        dbManager.setPairActive(dbManager.getGameSessionLatest(), activePair2.getID());
+        dbManager.setPairActive(dbManager.getGameSessionLatest(), activePair3.getID());
+        dbManager.setPairActive(dbManager.getGameSessionLatest(), activePair4.getID());
+
+        dbManager.getGameSessionLatest().createGroups(new VrcScorecardGenerator(), new VrcTimeSelection());
 
         AccountDatabaseHandler accountDatabaseHandler = new AccountDatabaseHandler(dbManager);
         AccountManager am = new AccountManager(accountDatabaseHandler);
 
         am.registerUserWithPlayer(AccountManager.DEMO_EMAIL, AccountManager.DEMO_PASSWORD,
-                activePair.getPlayers().get(0).getID(),
+                activePair1.getPlayers().get(0).getID(),
                 AccountManager.DEMO_SECURITY_QUESTION, AccountManager.DEMO_SECURITY_ANSWER);
     }
 
@@ -54,6 +66,26 @@ public class NotificationManagerTest {
         GameSession gameSession = new GameSession(newLadder);
         dbManager.persistEntity(gameSession);
         return gameSession;
+    }
+
+    /**
+     * Use this test to send a test email to your account. Before running this test, change the
+     * email of the user registered in the setUp method from AccountManager.DEMO_EMAIL to your
+     * email.
+     */
+    @Ignore
+    @Test
+    public void sendEmail() throws Exception {
+        Calendar testTime = Calendar.getInstance();
+        testTime.add(Calendar.MILLISECOND, 500);
+
+        Notifier emailNotifier = new EmailNotifier(new SimpleComposer());
+
+        NotificationManager notificationManager = new NotificationManager(dbManager,
+                testTime.getTime(), NotificationManager.PERIOD_ONE_WEEK);
+        notificationManager.scheduleEmailNotifications(emailNotifier);
+
+        Thread.sleep(1000000);
     }
 
     @Test
