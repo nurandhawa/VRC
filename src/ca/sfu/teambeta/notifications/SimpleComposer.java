98f66d56d4a1f25609a3390f4076a7b6f075b017
Alex Land <aland@sfu.ca>
2016-10-16 17:45:41 -0700

    Show scorecard in email notification
    The other pairs that are playing in a user's group will appear in the
    email notification.

--- src/ca/sfu/teambeta/notifications/SimpleComposer.java
+++ src/ca/sfu/teambeta/notifications/SimpleComposer.java
@@ -2,7 +2,9 @@ package ca.sfu.teambeta.notifications;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
+import ca.sfu.teambeta.core.Pair;
 import ca.sfu.teambeta.core.Player;
 import ca.sfu.teambeta.core.Scorecard;
 import ca.sfu.teambeta.core.User;
@@ -15,7 +17,8 @@ public class SimpleComposer implements Composer {
     private static final String MESSAGE_TEMPLATE = "Hi %s, \n\n"
             + "You're scheduled to play in the VRC Doubles "
             + "Badminton Ladder today at %s.\n\n"
-            + "VRC";
+            + "The other pairs in your group are:\n%s\n\n"
+            + "See you tonight!\nVRC";
 
     @Override
     public String composeSubject(User user, Scorecard scorecard) {
@@ -30,7 +33,30 @@ public class SimpleComposer implements Composer {
         Player player = user.getAssociatedPlayer();
         String playerName = player.getFirstName();
         String playTime = scorecard.getTimeSlot().getTimeString();
+        String otherPairsList = createPairList(player, scorecard);
 
-        return String.format(MESSAGE_TEMPLATE, playerName, playTime);
+        return String.format(MESSAGE_TEMPLATE, playerName, playTime, otherPairsList);
+    }
+
+    private String createPairList(Player currentPlayer, Scorecard scorecard) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Pair pair: scorecard.getPairs()) {
+            if (!pair.hasPlayer(currentPlayer)) {
+                stringBuilder.append(" - ");
+
+                List<Player> players = pair.getPlayers();
+                Player player1 = players.get(0);
+                Player player2 = players.get(1);
+
+                stringBuilder.append(player1.getFirstName()).append(" ").append(player1.getLastName());
+                stringBuilder.append(" and ");
+                stringBuilder.append(player2.getFirstName()).append(" ").append(player2.getLastName());
+
+                stringBuilder.append("\n");
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
